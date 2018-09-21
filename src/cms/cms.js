d00603f26b85e846d468fda0d3547193d0670b9f
Sally Northmore <sally.northmore@gmail.com>
2018-09-21 11:41:05 +0100

    Fix missing cms preview files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,12 @@
-import CMS from 'netlify-cms'
+import CMS from 'netlify-cms';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview';
+// import BlogPostPreview from './preview-templates/BlogPostPreview'
+// import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ProjectPagePreview from './preview-templates/ProjectPagePreview';
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview);
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+CMS.registerPreviewTemplate('project', ProjectPagePreview);
