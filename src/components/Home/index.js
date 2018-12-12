da0c1f5242f1b3a1783912c2bcb0c5ac07fac0c1
Sally Northmore <sally.northmore@gmail.com>
2018-12-12 15:54:07 +0000

    Update eslint, add generator

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Element, scroller } from 'react-scroll';
-
+import randomize from 'randomatic';
+import { Trail, animated, config } from 'react-spring';
 import Grid from '../Grid';
 import Message from '../Message';
 import Footer from '../Footer';
@@ -9,7 +9,35 @@ import { HomeComponent, Headline, Title, Projects } from './styles';
 
 import {} from './styles';
 
-class Home extends React.Component {
+const AnimatedText = items => {
+	return (
+<div className="AnimatedText">
+<Trail
+config={config.gentle}
+native
+keys={() => randomize('Aa', 20)}
+items={items}
+from={{ opacity: 0, x: -100 }}
+to={{ opacity: 1, x: 0 }}
+>
+{item => ({ x, opacity }) => (
+<animated.div
+key={randomize('Aa', 100)}
+className="AnimatedText-text"
+style={{
+ opacity,
+ transform: x.interpolate(x => `translate3d(0,${x}%,0)`),
+}}
+>
+{item}
+</animated.div>
+)}
+</Trail>
+</div>
+)
+};
+
+class Home extends Component {
 	state = {
 		isContact: false,
 		contactName: '',
@@ -34,11 +62,15 @@ class Home extends React.Component {
 	}
 
 	scrollTo = () => {
-		scroller.scrollTo('scroll-to-element', {
-			duration: 800,
-			delay: 0,
-			smooth: 'easeInOutQuart',
-		});
+		// scroller.scrollTo('scroll-to-element', {
+		// 	duration: 800,
+		// 	delay: 0,
+		// 	smooth: 'easeInOutQuart',
+		// });
+	};
+
+	createString = string => {
+		return string.split('');
 	};
 
 	render() {
@@ -48,6 +80,7 @@ class Home extends React.Component {
 		const messageText = isContact
 			? `Thanks for your message, ${contactName}`
 			: null;
+		const textArray = this.createString("I‘m a freelance frontend web developer. I live and work in London");
 
 		return (
 			<HomeComponent>
@@ -55,12 +88,11 @@ class Home extends React.Component {
 
 				<Headline>
 					<span>
-						I&rsquo;m a freelance frontend web developer living and working in
-						London
+						<AnimatedText items={textArray} />
 					</span>
 				</Headline>
 
-				<Element name="scroll-to-element">
+				<div name="scroll-to-element">
 					<Projects
 						ref={section => {
 							this.Projects = section;
@@ -70,7 +102,7 @@ class Home extends React.Component {
 
 						<Grid items={projects} />
 					</Projects>
-				</Element>
+				</div>
 
 				<Footer to="scroll-to-element" handleClick={this.scrollTo} />
 			</HomeComponent>
