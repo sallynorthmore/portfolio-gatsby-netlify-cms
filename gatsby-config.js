dedb4e83b1db921684a8d9c15a6b27529f95a504
Sally Northmore <sally.northmore@gmail.com>
2018-12-13 14:57:46 +0000

    Add ga tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,12 @@ module.exports = {
 				modulePath: `${__dirname}/src/cms/cms.js`,
 			},
 		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-130953073-1',
+			},
+		},
 		'gatsby-plugin-netlify', // make sure to keep it last in the array
 	],
 };
