a88cb4251faa43e78612a80ce3a75915fd854680
Sally Northmore <snorthmore@brilliantbasics.com>
2019-09-07 13:04:33 +0100

    Add badge

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -2,4 +2,11 @@ import styled from 'styled-components';
 
 export const LayoutComponent = styled.div`
 	padding: 2rem 1rem;
+
+	.ppmBadge {
+			position: fixed;
+			bottom: 0;
+			right: 0;
+			z-index: 4;
+		}
 `;
